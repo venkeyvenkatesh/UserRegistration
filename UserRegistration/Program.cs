ab1a632f6f5024d1392621aec12b6544eeeced08
venkey <venkeyvenkatesh885@gmail.com>
2020-10-05 11:06:43 +0530

    [venkey]Refactor. Validated thr first name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
@@ -6,7 +7,39 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
+            //variables
+            string firstName;
+
             Console.WriteLine("Welcome to user Registration problem");
+
+            Console.WriteLine("Enter your first name: ");
+            firstName = Console.ReadLine();
+
+           while(!(validateFirstName(firstName)))
+            {
+                Console.WriteLine("Please enter valid first Name ");
+                firstName = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("You have entered valid first name ");
+            
+        }
+
+        public static bool validateFirstName(string firstName)
+        {
+            string pattern = @"(^[A-Z]{1}[a-z]{2,}$)";
+
+            Regex re = new Regex(pattern);
+            if(re.IsMatch(firstName))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
 }
