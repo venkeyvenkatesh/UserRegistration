aa68e825e90bca04ac3624c7c6c1cf92552284ae
venkey <venkeyvenkatesh885@gmail.com>
2020-10-05 17:29:16 +0530

    [venkey] Refactor. checking password to contain exactly one specialchar

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -5,6 +5,10 @@ namespace UserRegistration
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             //variables
@@ -14,9 +18,11 @@ namespace UserRegistration
 
              Validation obj = new Validation();
 
+            //getting firstName input from user
             Console.WriteLine("Enter your first name: ");
             firstName = Console.ReadLine();
 
+            //valdating the fisrtName
             while (!(obj.validateName(firstName)))
             {
                 Console.WriteLine("Please enter valid first Name ");
@@ -24,9 +30,12 @@ namespace UserRegistration
 
             }
 
+
+            //getting lastName as input from user
             Console.WriteLine("Enter your Last name: ");
             lastName = Console.ReadLine();
 
+            //valdating the lastName
             while (!(obj.validateName(lastName)))
             {
                 Console.WriteLine("Please enter valid last Name ");
@@ -34,9 +43,12 @@ namespace UserRegistration
 
             }
 
+
+            //getting EmailAddress as input from user
             Console.WriteLine("Enter your Email address: ");
             emailId = Console.ReadLine();
 
+            //valdating the EmailAddress
             while (!(obj.validateEmailId(emailId)))
             {
                 Console.WriteLine("Please enter valid Email id ");
@@ -46,9 +58,11 @@ namespace UserRegistration
 
 
 
+            //getting phoneNumber as input from user
             Console.WriteLine("Enter your phone Number : ");
             phoneNumber = Console.ReadLine();
 
+            //valdating the phoneNumber
             while (!(obj.validatePhoneNumber(phoneNumber)))
             {
                 Console.WriteLine("Please enter valid phone number");
@@ -56,18 +70,21 @@ namespace UserRegistration
 
             }
 
-           
-                Console.WriteLine("Enter your password : ");
+
+
+            //getting password as input from user
+            Console.WriteLine("Enter your password : ");
                 password = Console.ReadLine();
 
-                while (!(obj.validatePassword(password)))
+            //valdating the password
+            while (!(obj.validatePassword(password)))
                 {
                     Console.WriteLine("Please enter valid password");
                     password = Console.ReadLine();
 
                 }
 
-            
+            Console.WriteLine("Registration successful");
 
         }
 
--- UserRegistration/Validation.cs
+++ UserRegistration/Validation.cs
@@ -8,6 +8,11 @@ namespace UserRegistration
     class Validation
     {
 
+        /// <summary>
+        /// Validates the name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
         public bool validateName(string name)
         {
             string pattern = @"(^[A-Z]{1}[a-z]{2,}$)";
@@ -24,6 +29,11 @@ namespace UserRegistration
 
         }
 
+        /// <summary>
+        /// Validates the email identifier.
+        /// </summary>
+        /// <param name="emailId">The email identifier.</param>
+        /// <returns></returns>
         public bool validateEmailId(string emailId)
         {
             string pattern = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
@@ -39,6 +49,11 @@ namespace UserRegistration
             }
         }
 
+        /// <summary>
+        /// Validates the phone number.
+        /// </summary>
+        /// <param name="phoneNumber">The phone number.</param>
+        /// <returns></returns>
         public bool validatePhoneNumber(string phoneNumber)
         {
             string pattern = @"(^[1-9]{1,}[0-9\\-]{0,}[ ]{1}[1-9]{1}[0-9]{9}$)";
@@ -54,9 +69,14 @@ namespace UserRegistration
             }
         }
 
+        /// <summary>
+        /// Validates the password.
+        /// </summary>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
         public bool validatePassword(string password)
         {
-            string pattern = @"(^(?=.*[A-Z])(?=.*[0-9])([A-Za-z0-9!@#$%&*()-+=^]){8,}$)";
+            string pattern = @"(^(?=.{8,}$)((?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]*[!@#$^&*()-+=]{1}[a-zA-Z0-9]*))$)";
 
             Regex re = new Regex(pattern);
             if (re.IsMatch(password))
